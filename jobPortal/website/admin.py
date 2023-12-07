8007eddc5d691c0a628f22338a17a613568a5e2b
WeeklySorcery <lioneerode@gmail.com>
2023-12-07 21:26:27 +0800

    profile improvement

--- jobPortal/website/admin.py
+++ jobPortal/website/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import UserProfile
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'gender', 'address', 'phone_number', 'status']
+    # Add more options as needed
+
+admin.site.register(UserProfile, UserProfileAdmin)
--- jobPortal/website/models.py
+++ jobPortal/website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')], null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    status = models.CharField(max_length=50, null=True, blank=True)
+    # Add more fields as needed
+
+    def __str__(self):
+        return self.user.username
